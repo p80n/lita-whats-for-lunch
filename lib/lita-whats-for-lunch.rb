8b33d919300e6a3f1cf4b344479fe63aa8ccdc4b
Peyton Vaughn <pvaughn@6fusion.com>
2017-05-23 19:01:40 -0400

    wip

--- lib/lita-whats-for-lunch.rb
+++ lib/lita-whats-for-lunch.rb
@@ -4,6 +4,8 @@ Lita.load_locales Dir[File.expand_path(
   File.join("..", "..", "locales", "*.yml"), __FILE__
 )]
 
+require 'lita-whats-for-lunch/restaurants'
+
 require "lita/handlers/whats_for_lunch"
 
 Lita::Handlers::WhatsForLunch.template_root File.expand_path(
